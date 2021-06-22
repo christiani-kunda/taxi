1d1db3a68a371209bee7027b7962d6dc9d04daf4
christian <kundafils@gmail.com>
2021-06-22 16:20:39 +0200

    added the tests file

---
+++ src/test/java/demo/taxi/com/RestServerTests.java
@@ -0,0 +1,13 @@
+package demo.taxi.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
