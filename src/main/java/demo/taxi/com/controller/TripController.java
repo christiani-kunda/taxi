a4b0c51695e0c074b7053e8f7cab7fda78f26790
christian <kundafils@gmail.com>
2021-06-23 11:12:54 +0200

    implemented the apis solely for the trip

--- src/main/java/demo/taxi/com/controller/TripController.java
+++ src/main/java/demo/taxi/com/controller/TripController.java
@@ -1,7 +1,14 @@
 package demo.taxi.com.controller;
 
+import demo.taxi.com.model.Trip;
+import demo.taxi.com.service.ITripService;
+import demo.taxi.com.util.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * The Class ITripController.
@@ -12,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("trip")
 public class TripController {
 
+	@Autowired
+	private ITripService tripService;
+
+	@GetMapping("/all-active-trips")
+	public Response<List<Trip>> getListOfActiveTrips(){
+		return tripService.getListOfActiveTrips();
+	}
 }
