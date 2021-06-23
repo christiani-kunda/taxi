08f8e483d572c0d8fa1e4c737dd8f26b86f357ee
christian <kundafils@gmail.com>
2021-06-23 10:00:14 +0200

    started the implementation of rest apis

---
+++ src/main/java/demo/taxi/com/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package demo.taxi.com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The Class IApplicationController.
+ *
+ * @author Christian Iradukunda
+ */
+@RestController
+@RequestMapping("request")
+public class ApplicationController {
+
+}
---
+++ src/main/java/demo/taxi/com/controller/TripController.java
@@ -0,0 +1,15 @@
+package demo.taxi.com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The Class ITripController.
+ *
+ * @author Christian Iradukunda
+ */
+@RestController
+@RequestMapping("trip")
+public class TripController {
+
+}
